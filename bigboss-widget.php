7cb29da33ceaee526ca74cda33b4aa969572a824
Bulbul bigboss <bulbulbigboss@gmail.com>
2016-04-05 00:41:02 +0600

    Added files via upload

---
+++ bigboss-widget.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Plugin Name: Bigboss Recent Post Widget
+* Plugin URI: http://wordpress.org/plugins/bigboss-recent-post-widget
+* Description: Bigboss Recent Post Widget for creating Advance Recent post section in Sidebar or in Any place using shortcode.
+* Version: 1.1
+* Author: Bulbul bigboss
+* Author URI: http://www.bigbosstheme.com/bigboss
+* Tags:Latest-news,Advance recent post,Recent post shortcode, Recent post Setting, cpt slide show, post types, responsive slider, cpt responsive slider 
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+
+//
+
+
+?>
