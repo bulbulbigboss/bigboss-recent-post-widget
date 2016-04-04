e4402d3ac353ac0a14d91324cbccba82fdc647e3
Bulbul bigboss <bulbulbigboss@gmail.com>
2016-04-05 02:36:14 +0600

    Added files via upload

--- bigboss-widget.php
+++ bigboss-widget.php
@@ -14,5 +14,61 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 //
 
+class BbWidgetArea extends WP_Widget{
+	
+	
+	public function __construct(){
+			parent::__construct(false,'Bigboss Recent Post Widget', array(
+			
+					'description' => 'Bigboss Recent Post widget is advance Latest Post widget Section' 
+			));
+		
+		}
+		
+		
+		
+	public function widget($args, $instance){
+		
+		$title = $instance['title'];
+		
+		echo $args['before_widget'] .$args['before_title'] . $title .$args['after_title'] . $args['after_widget'] ;
+		
+		
+		
+		
+		}
+		
+		
+	public function form($instance){
+		$title = $instance['title'];
+		
+		?>
+    
+    <p>
+    	<label for="<?php $this->get_field_id('title');?>">Title</label>
+        <input type="text" name="<?php echo $this->get_field_name('title');?>" id="<?php $this->get_field_id('title');?>" class="widefat" value="<?php echo $title ; ?>" />
+    
+    </p>
+    
+    
+		
+		
+		<?php }
+	
+	
+	
+	}//end class BbWidgetArea here
 
-?>
+function registerBBwidget(){
+	register_widget('BbWidgetArea');
+	
+	}
+add_action('widgets_init','registerBBwidget');
+	
+	
+	
+	
+	
+	
+	
+	
